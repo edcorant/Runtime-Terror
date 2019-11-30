36b361256befaaee8cd41d4311669bc6ec8ef891
m-knight99 <57108366+m-knight99@users.noreply.github.com>
2019-11-30 10:16:48 -0500

    Add files via upload

--- action_page1.php
+++ action_page1.php
@@ -1,19 +1,36 @@
+<body bgcolor="#a4afa0">
 <?php
 
-    $fName = $_POST['FirstName'];
-    echo "<br>" . $fName;
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "insurance_match";
+
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
     
+    // inserting into Patient
+    $fName = $_POST['FirstName'];
     $lInit = $_POST['LastInitial'];
-    echo "<br>" . $lInit;
+    $name = $fName . " " . $lInit;
     
     $drType = $_POST['DoctorType'];
-    echo "<br>" . $drType;
 
     $locationChosen = $_POST['Location'];
-    echo "<br>" . $locationChosen;
     
-    // the above code is printing the values selected from the insert function
+    $sql = "SELECT * FROM Patient;"; // finding the next P_id
+    $result = mysqli_query($conn, $sql);
+    $nextPid = mysqli_num_rows($result);
     
+    $sql = "INSERT INTO Patient (`City`, `P_id`, `P_Name`, `Health_Issue`) VALUES ('$locationChosen', '$nextPid', '$name', '$drType');";
+    $result2 = mysqli_query($conn, $sql);
+   
+    // inserting into CoveredBy
+    $insur = $_POST['insuranceComp'];
+    $sql = "INSERT INTO CoveredBy (`C_id`, `P_id`) VALUES ('$insur', '$nextPid');";
+    $result3 = mysqli_query($conn, $sql);
     
-
+    echo "Patient has been inserted. Your unique patient ID is ";
+    echo $nextPid;
+    echo ". Keep this for your records.";
+    
 ?>
