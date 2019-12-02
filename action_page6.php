3de81b2f99ae03609f2ed45c33f7dc564c091016
m-knight99 <57108366+m-knight99@users.noreply.github.com>
2019-12-02 12:06:35 -0500

    Add files via upload

--- action_page6.php
+++ action_page6.php
@@ -1,5 +1,22 @@
+<body bgcolor="#a4afa0">
 <?php
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "insurance_match";
+    
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    
     $dSpecialty = $_POST['DoctorType'];
-    echo "<br>" . $dSpecialty;
-// Printing the specialty for the doctor/patient join
+    
+    $sql = "SELECT P_Name, D_Name FROM Patient, Doctor WHERE Patient.Health_Issue = '$dSpecialty' AND Doctor.Specialty = '$dSpecialty';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    echo "<br>" . $resultCheck . " doctor and patient matches found having specialty = " . $dSpecialty . ". <br>";
+    echo "<br>";
+    while($row = mysqli_fetch_array($result)) // code works; try to figure out how to print better
+    {
+       print_r($row);
+    }
+// doctor/patient join function
 ?>
--- action_page7.php
+++ action_page7.php
@@ -1,5 +1,23 @@
+<body bgcolor="#a4afa0">
 <?php
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "insurance_match";
+    
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    
     $dpLocation = $_POST['Location'];
     echo "<br>" . $dpLocation;
+    
+    $sql = "SELECT P_Name, D_Name FROM Patient, Doctor WHERE Patient.City = '$dpLocation' AND Doctor.Location = '$dpLocation';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    echo "<br>" . $resultCheck . " doctor and patient matches found having location = " . $dpLocation . ". <br>";
+    echo "<br>";
+    while($row = mysqli_fetch_array($result)) // code works; try to figure out how to print better
+    {
+       print_r($row);
+    }
 // printing the location of the patient/doctor join
 ?>
