2a21fa07cb55e67d77d0b6b943368d0c5ede1f01
m-knight99 <57108366+m-knight99@users.noreply.github.com>
2019-12-02 10:04:41 -0500

    Add files via upload

--- action_page5.php
+++ action_page5.php
@@ -1,8 +1,25 @@
+<body bgcolor="#a4afa0">
 <?php
-    $dName = $_POST['FirstName'];
-    echo "<br>" . $dName;
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "insurance_match";
     
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    
+    $dName = $_POST['FirstName'];
     $lName = $_POST['LastName'];
-    echo "<br>" . $lName;
-// printing the doctor name for the search doctor function
+    $name = $dName . " " . $lName;
+
+    $sql = "SELECT D_Name FROM Doctor WHERE D_Name = '$name';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck == 1)
+    {
+        echo $resultCheck ." doctor " . " found named " . $name . ".";
+    }
+    else
+    {
+        echo $resultCheck ." doctors " . " found named " . $name . ".";
+    }
 ?>
